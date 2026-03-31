16b0754b5702083fccccb95266724c2b7fd43c3a
russcraftscode <153451301+russcraftscode@users.noreply.github.com>
2026-03-30 19:53:06 -0700

    Add CLI commands to taiga-parser.py

--- taiga-parser.py
+++ taiga-parser.py
@@ -1,14 +1,32 @@
 import json
 import sys
+import os
+import csv
 from pprint import pprint
 
-with open("project.json" , "r")as proj_file:
+
+json_filename = None
+
+if len(sys.argv) > 1: # set the json filename to the user supplied one
+    json_filename = sys.argv[1]
+else: # get a json file out of the current dir
+    print("No filename given, picking 1st json file in directory")
+    filenames = os.listdir()
+    for filename in filenames:
+        if ".json" in filename:
+            json_filename = filename
+
+if not json_filename: # if no json file found
+    print("Error: Unable to find json project file")
+    sys.exit()
+print(f"Scraping data from file: {json_filename}")
+
+with open(json_filename , "r") as proj_file:
     proj_json = json.load(proj_file)
 
 task_url_start = "https://tree.taiga.io/project/" + proj_json["slug"] + "/task/"
 
-# get the user story numbes and thier total point values
-
+# get the user story numbers and their total point values
 user_stories_json = proj_json["user_stories"]
 user_stories = {}
 
@@ -27,16 +45,11 @@ for line in user_stories_json:
         points += int (line["role_points"][4]["points"])
     user_stories[story_number] = points
 
-print(f"Found {len(user_stories_json)} tasks")
-
-
+print(f"Found {len(user_stories_json)} user stories")
 
 # extract info about tasks
 tasks_json = proj_json["tasks"]
 tasks = []
-
-pprint(user_stories_json[0])
-#pprint(tasks_json[0])
 
 for line in tasks_json:
     task = {
@@ -53,3 +66,34 @@ for line in tasks_json:
     tasks.append(task)
 
 print(f"Extracted {len(tasks)} tasks")
+
+# identify the team members
+team_members = []
+for task in tasks: # make a list of all unique team members
+    if task["assigned"] not in team_members and task["assigned"]:
+        team_members.append(task["assigned"])
+print(f"Identified {len(team_members)} team members")
+print(team_members)
+
+# Sprint	User Story	Story Points	Link to Task	Coding Task?	tapsey	casteri1	alewi104	rjohn172	bmarti65
+
+# generate lines for the csv output
+lines = []
+for task in tasks:
+    if task["story_number"]:
+        line = ["sprint number", "US"+str(task["story_number"]), task["link"], task["points"], " "]
+    else:
+        line = ["sprint number", "Storyless Task", task["link"], task["points"], " "]
+    for team_member in team_members: # put 100% in the proper column
+        if team_member == task["assigned"]:
+            line.append("100%")
+        else:
+            line.append("")
+    lines.append(line)
+
+with open('draft_work_report.csv', 'w', newline='') as csvfile:
+    fieldnames = ['Sprint', 'User Story', 'Points', 'Task Link', 'Coding Task'] + team_members
+    #writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer = csv.writer(csvfile)
+    writer.writerow(fieldnames)
+    writer.writerows(lines)
